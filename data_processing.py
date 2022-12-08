6403c0f30f5eb4229a7a856ed9cc69571f3d0ceb
misraii <51056931+misraii@users.noreply.github.com>
2022-12-07 21:17:59 -0800

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,41 @@
+import pandas as pd
 
+Collect and process news and sentiment data for Chainlink
+news_data = <code to collect and process news and sentiment data>
+
+Get the daily high and low prices for Chainlink from Coinbase
+high_low_data = client.get_product_24hr_stats("LINK-USD")
+high_price = high_low_data["high"]
+low_price = high_low_data["low"]
+
+Convert the data to a Pandas DataFrame
+data = pd.DataFrame(data)
+
+Add the daily high and low prices to the data
+data["high"] = high_price
+data["low"] = low_price
+
+Add the trading volume from Binance.US to the data
+data["binance_volume"] = binance_data
+
+Add the news and sentiment data to the data
+data["news_sentiment"] = news_data
+
+Clean and preprocess the data
+data = data.dropna()
+data["time"] = pd.to_datetime(data["time"])
+data["price"] = data["price"].astype(float)
+data["volume"] = data["volume"].astype(float)
+data["market_cap"] = data["market_cap"].astype(float)
+data["high"] = data["high"].astype(float)
+data["low"] = data["low"].astype(float)
+data["binance_volume"] = data["binance_volume"].astype(float)
+data["news_sentiment"] = data["news_sentiment"].astype(float)
+
+Normalize the data
+data["price"] = (data["price"] - data["price"].mean()) / data["price"].std()
+data["volume"] = (data["volume"] - data["volume"].mean()) / data["volume"].std()
+data["market_cap"] = (data["market_cap"] - data["market_cap"].mean()) / data["market_cap"].std()
+data["high"] = (data["high"] - data["high"].mean()) / data["high"].std()
+data["low"] = (data["low"] - data["low"].mean()) / data["low"].std()
+data["binance_volume"] = (data["binance_volume"] - data["binance_volume"].mean()) / data["binance_
